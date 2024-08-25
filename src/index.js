3f6729971051d3e46ddb72931eb3d8532ef3f922
Anca_Sab <asabadeanu@gmail.com>
2024-08-25 19:55:21 +0300

    Cleaning1

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ errorElement.style.visibility = 'hidden';
 
 async function fetchBreeds() {
   try {
-    // loaderElement.classList.remove('hidden');
+    
     loaderElement.style.visibility = 'visible';
-    errorElement.style.visibility = 'hidden';// Show loader while fetching breeds
+    errorElement.style.visibility = 'hidden';
     const response = await axios.get('https://api.thecatapi.com/v1/breeds?api_key=live_qZuYx3rGEnTsT5CEggwmg46wjLVKCaUJFGjXpuyIw9LbBP5gLRoPCpKY5ZTkPuKw');
     
     const breeds = response.data;
@@ -33,27 +33,21 @@ async function fetchBreeds() {
 
     });
 
-    // Hide loader and show select element after fetching breeds
-    // loaderElement.classList.add('hidden');
+   
     loaderElement.style.visibility = 'hidden';
-    // selectElement.classList.remove('hidden');
     selectElement.style.visibility = 'visible';
   } catch (error) {
-    console.error('Error fetching breeds:', error);
     Notiflix.Notify.failure('Failed to fetch breeds. Please try again later.');
-    // Show error message and hide loader
-    // errorElement.classList.remove('hidden');
+   
     errorElement.style.visibility = 'visible';
-    // loaderElement.classList.add('hidden');
     loaderElement.style.visibility = 'hidden';
   }
 }
 
 async function fetchCatByBreed(breedId) {
   try {
-    // loaderElement.classList.remove('hidden'); // Show loader while fetching cat data
+  
     loaderElement.style.visibility = 'visible';
-    // errorElement.classList.add('hidden'); // Hide error message initially
     errorElement.style.visibility = 'hidden';
 
     const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_qZuYx3rGEnTsT5CEggwmg46wjLVKCaUJFGjXpuyIw9LbBP5gLRoPCpKY5ZTkPuKw`);
@@ -67,27 +61,18 @@ async function fetchCatByBreed(breedId) {
       <p>Temperament: ${catData.breeds[0].temperament}</p>
     `;
 
-    // Hide loader and show cat info after successful fetch
-    // loaderElement.classList.add('hidden');
     loaderElement.style.visibility = 'hidden'
-    // catInfoElement.classList.remove('hidden');
-    catInfoElement.style.visibility = 'visible';
+    // catInfoElement.style.visibility = 'visible';
+
   } catch (error) {
-    console.error('Error fetching cat data:', error);
     Notiflix.Notify.failure('Failed to fetch cat information. Please try again later.');
-    // errorElement.classList.remove('hidden'); // Show error message on error
+   
     errorElement.style.visibility = 'visible';
   } finally {
-    // Ensure loader is hidden in case of errors
-    // loaderElement.classList.add('hidden');
     loaderElement.style.visibility = 'hidden'
   }
-}
+};
 
-// function hiddenMessage(element) {
-//   if element.querySelector('hidden');
-//   hiddenElement.style.visibility = 'hidden'; 
-// }
 
 selectElement.addEventListener('change', (event) => {
   const selectedBreedId = event.target.value;
